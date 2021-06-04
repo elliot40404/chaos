637c95cee91995bcaa16f05c3aedbc72fe870443
elliot404 <fortesting40404@gmail.com>
2021-06-04 10:52:46 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,14 @@
 </template>
 
 <style lang="scss">
+* {
+  padding: 0%;
+  margin: 0%;
+  box-sizing: border-box;
+}
+body {
+  background: #000;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -14,14 +22,11 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
-
     &.router-link-exact-active {
       color: #42b983;
     }
---
+++ src/components/Code.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <section>
+      <v-ace-editor
+        v-model:value="content"
+        @init="editorInit"
+        :lang="lang"
+        theme="solarized_dark"
+        :style="code"
+        :options="options"
+      />
+      <div class="out">{{ content }}</div>
+    </section>
+    <!-- <button>👋 Hello</button> -->
+    <select v-model="language" name="select" id="select">
+      <option value="javascript">javascript</option>
+      <option value="python">python</option>
+      <option value="html">html</option>
+    </select>
+    {{ lang }}
+  </div>
+</template>
+
+<script>
+import { VAceEditor } from "vue3-ace-editor";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-solarized_dark";
+import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/ext-language_tools";
+
+export default {
+  name: "Code",
+  data() {
+    return {
+      lang: "javascript",
+      content: "",
+      code: {
+        flex: 1,
+        height: "300px",
+        width: "400px",
+      },
+      options: {
+        fontSize: 16,
+        highlightActiveLine: true,
+        showLineNumbers: true,
+        tabSize: 4,
+        showPrintMargin: false,
+        showGutter: true,
+        enableBasicAutocompletion: true,
+        enableLiveAutocompletion: true,
+        enableSnippets: true
+      },
+    };
+  },
+  components: {
+    VAceEditor,
+  },
+  methods: {
+    editorInit() {
+      return true;
+    },
+  },
+  computed: {
+    language: {
+      get() {
+        return this.lang;
+      },
+      set(value) {
+        this.lang = value;
+      },
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+button {
+  padding: 0.6em 2em;
+  border-radius: 10px;
+  background: rgb(86, 86, 255);
+  font-size: 20px;
+  border: none;
+  outline: none;
+  color: #fff;
+  font-weight: bold;
+  cursor: pointer;
+}
+section {
+  width: 100%;
+  display: flex;
+  gap: 20px;
+}
+.out {
+  flex: 1;
+  background: rgb(2, 0, 32);
+  color: yellow;
+  padding: 10px;
+  text-align: left;
+}
+</style>
